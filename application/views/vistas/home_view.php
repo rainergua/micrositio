70879638d69e2bdac66d665f6f73382d3f06af1f
rainergua <rainergua@gmail.com>
2020-03-28 19:48:54 -0400

    Adicion de Cards en laparte inferior

--- application/views/vistas/home_view.php
+++ application/views/vistas/home_view.php
@@ -2,31 +2,79 @@
     <div class="row">
         <div class="col-6">
             <div class="jumbotron">
-            <form>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Nombre Completo</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre Completo">
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Example textarea</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Enviar consulta</a>
-                </p>
+                <p class="lead">Por favor remita su consulta, y le responderemos a la brevedad posible.</p>
+                <hr class="my-4">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Nombre Completo</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre Completo">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Escriba su mensaje</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                    </div>
+                    <p class="lead">
+                        <a class="btn btn-secondary btn-lg" href="#" role="button">Enviar consulta</a>
+                    </p>
                 </form>
             </div>
         </div>
         <div class="col-6">
             <div class="jumbotron">
-                <h1 class="display-4">Formulario 2</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" target="_blank" href="https://web.whatsapp.com/send?phone=59167325245&text=Hola!%20Quiero%20generar%20mas%20ventas!" role="button">Enviar mensaje</a>
-                </p>
+                <h4 class="display-4">Respuestas</h4>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et minus repellendus excepturi facilis possimus nostrum quasi blanditiis corrupti cupiditate culpa.</p>
+                        <p class="card-text"><small class="text-muted">Actulizado hace 5 mins.</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Me siento agoviado por no poder salir</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quos consectetur laborum sit culpa illum maxime fugit ab illo debitis!.</p>
+                        <p class="card-text"><small class="text-muted">Actulizado hace 5 mins.</small></p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-4">
+            <div class="card text-white bg-danger mb-4" style="max-width: 24rem;">
+                <div class="card-header">Ir a Preguntas Recientes</div>
+                <div class="card-body">
+                    <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                    <h5 class="card-title">Me siento agoviado por no poder salir</h5>
+                    <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                    <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card text-white bg-warning mb-4" style="max-width: 24rem;">
+                <div class="card-header">Ir a Preguntas Frecuentes</div>
+                <div class="card-body">
+                <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                    <h5 class="card-title">Me siento agoviado por no poder salir</h5>
+                    <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                    <h5 class="card-title">¿Que puedo hacer si me contagio?</h5>
+                </div>
+                </div>
+            </div>
+        <div class="col-4">
+            <div class="card text-white bg-success mb-4" style="max-width: 24rem;">
+                <div class="card-header">Consulta directa - WhatsApp</div>
+                <div class="card-body">
+                    <h5 class="card-title">715-12345 - Especialista 1</h5>
+                    <h5 class="card-title">715-54321 - Especialista 2</h5>
+                    <h5 class="card-title">715-22222 - Especialista 3</h5>
+                    <h5 class="card-title">715-33333 - Especialista 4</h5>
+                    <h5 class="card-title">715-22222 - Especialista 5</h5>
+                    <h5 class="card-title">715-33333 - Especialista 6</h5>
+                    <h5 class="card-title">715-33333 - Especialista 7</h5>
+                </div>
+            </div>
+        </div>
+    </div><!--ROW-->
 </div>

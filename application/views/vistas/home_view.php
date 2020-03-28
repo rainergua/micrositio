5af9ce210e0db792b4f332927cdc50719a95e1ae
rainergua <rainergua@gmail.com>
2020-03-28 18:53:21 -0400

    Reduccion 2 joombutrons

--- application/views/vistas/home_view.php
+++ application/views/vistas/home_view.php
@@ -1,54 +1,32 @@
 <div class="container">
-<div class="row">
-<div class="col  text-center">
-<div class="row">
-    <div class="col-6">
-        <div class="jumbotron">
-            <h1 class="display-4">Formulario 1</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
+    <div class="row">
+        <div class="col-6">
+            <div class="jumbotron">
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Nombre Completo</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre Completo">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Enviar consulta</a>
+                </p>
+                </form>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="jumbotron">
+                <h1 class="display-4">Formulario 2</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" target="_blank" href="https://web.whatsapp.com/send?phone=59167325245&text=Hola!%20Quiero%20generar%20mas%20ventas!" role="button">Enviar mensaje</a>
+                </p>
+            </div>
         </div>
     </div>
-    <div class="col-6">
-        <div class="jumbotron">
-            <h1 class="display-4">Formulario 2</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-6">
-    <div class="jumbotron">
-            <h1 class="display-4">Formulario 3</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
-        </div>
-    </div>
-    <div class="col-6">
-    <div class="jumbotron">
-            <h1 class="display-4">Formulario 4</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
-        </div>
-    </div>
-</div>
-</div>
-</div>
 </div>
